8bbb688bbb1424559ea21e37aa9694fb8bb314c0
ihsan <ihsan.nurdiansyah@emveep.com>
2019-10-10 10:38:47 +0700

    Initial Commit

---
+++ TheMovie/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TheMovie
+//
+//  Created by dodets on 10/10/19.
+//  Copyright © 2019 dodets. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
